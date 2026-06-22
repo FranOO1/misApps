0bbff2d65efdb71468b5252d759a54f896b13a7c
FranOO1 <francis.olea88@gmail.com>
2026-06-22 16:24:27 +0200

    Delete plantómetro directory

--- plantómetro/sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (e) => {
-    console.log('[Plant Guardian] Service Worker instalado');
-});
-
-self.addEventListener('fetch', (e) => {
-    // Esto es un trabajador pasivo solo para cumplir el requisito de instalación de Chrome
-});
